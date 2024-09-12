5a4a3528ccac5e21cccb7be4e4b3514b7298400f
msiwy12345 <mateuszsiwy00@gmail.com>
2024-09-12 15:15:11 +0200

    Add project files.

---
+++ Phone Book Entity/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+public class ContactContext: DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\Practice;Database=contacts;Trusted_Connection=True;");
+}
+
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+
+}
